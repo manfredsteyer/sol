95b2ad3e930ad902197f0fd570157a192cabbcb0
ManfredSteyer <manfred.steyer@gmx.net>
2017-06-14 13:12:55 +0200

    bonus: edit passenger

--- src/app/flight-booking/passenger-edit/passenger-edit.component.ts
+++ src/app/flight-booking/passenger-edit/passenger-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Passenger } from '../../entities/passenger';
+import { PassengerService } from '../passenger.service';
 
 @Component({
   selector: 'app-passenger-edit',
@@ -8,13 +10,41 @@ import { ActivatedRoute } from '@angular/router';
 export class PassengerEditComponent implements OnInit {
 
   id: string;
+  passenger: Passenger;
+  message: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private passengerService: PassengerService) { }
 
   ngOnInit() {
     this.route.params.subscribe(p => {
       this.id = p['id'];
+
+      this
+        .passengerService
+        .findById(this.id)
+        .subscribe(
+          p => { this.passenger = p; },
+          err => {console.error('Fehler beim Laden', err); }
+        );
+
     })
   }
 
+  save() {
+    this
+      .passengerService
+      .save(this.passenger)
+      .subscribe(
+        p => {
+          this.passenger = p;
+          this.message = 'Successfully saved!';
+        },
+        err => {
+          console.debug('Error during Saving', err);
+          this.message = 'Error: ' + err.text();
+        }
+      );
+  }
 }
--- src/app/flight-booking/passenger.service.ts
+++ src/app/flight-booking/passenger.service.ts
@@ -6,26 +6,51 @@ import { Observable } from "rxjs/Observable";
 @Injectable()
 export class PassengerService {
 
-    constructor(private http: Http) {
+  constructor(private http: Http) {
+  }
 
-    }
-    
-    find(name: string): Observable<Passenger[]> {
-        
-        let url = 'http://www.angular.at/api/passenger';
+  find(name: string): Observable<Passenger[]> {
 
-        let headers = new Headers();
-        headers.set('Accept', 'application/json');
+      let url = 'http://www.angular.at/api/passenger';
 
-        let search = new URLSearchParams();
-        search.set('name', name);
+      let headers = new Headers();
+      headers.set('Accept', 'application/json');
 
-        return this.http
-            .get(url, { headers, search})
-            .map(resp => resp.json());
+      let search = new URLSearchParams();
+      search.set('name', name);
+
+      return this.http
+          .get(url, { headers, search})
+          .map(resp => resp.json());
 
 
-    }
+  }
 
+  findById(id: string): Observable<Passenger> {
+
+    let url = 'http://www.angular.at/api/passenger';
+
+    let headers = new Headers();
+    headers.set('Accept', 'application/json');
+
+    let search = new URLSearchParams();
+    search.set('id', id);
+
+    return this.http
+      .get(url, { headers, search})
+      .map(resp => resp.json());
+  }
+
+  save(passenger: Passenger): Observable<Passenger> {
+
+    let url = 'http://www.angular.at/api/passenger';
+
+    let headers = new Headers();
+    headers.set('Accept', 'application/json');
+
+    return this.http
+      .post(url, passenger, { headers })
+      .map(resp => resp.json());
+  }
 
 }
